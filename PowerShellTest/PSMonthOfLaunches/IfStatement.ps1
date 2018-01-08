164d31e782bb0e1c95d471ddc3df1e20edeceba3
DESKTOP-HPRUNBD\Darek <darek.uchman@gmail.com>
2018-01-08 22:02:55 +0100

    Powershell in action

--- PowerShellTest/PSMonthOfLaunches/IfStatement.ps1
+++ PowerShellTest/PSMonthOfLaunches/IfStatement.ps1
@@ -21,7 +21,85 @@
 
 
 
-for($counter = 0; $counter -lt 10; $counter++)
-{
-Write-Host "Processing item $counter"
-}
+#for($counter = 0; $counter -lt 10; $counter++)
+#{
+#Write-Host "Processing item $counter"
+#}
+
+#$handleSum = 0;
+#foreach($process in Get-Process |Where-Object { $_.Handles -gt 500 })
+#{
+#$handleSum += $process.Handles
+#}
+#$handleSum
+
+
+#$command = "";
+#while($command -notmatch "quit")
+#{
+#$command = Read-Host "Enter your command"
+#}
+
+
+#$validResponses = "Yes","No"
+#$response = ""
+
+#do
+#{
+#$response = read-host "Yes or No?"
+#} while($validResponses -notcontains $response)
+#"Got it."
+#$response = ""
+
+
+#do
+#{
+#$response = read-host "Yes or No?"
+#} until($validResponses -contains $response)
+#"Got it."
+
+
+#for($counter = 0; $counter -lt 5; $counter++)
+#{
+#	for($counter2 = 0; $counter2 -lt 5; $counter2++)
+#	{
+#			if($counter2 -eq 2)
+#			{
+#				break
+#			}
+#			Write-Host "Processing item $counter,$counter2"
+#	}
+#}
+
+#[System.Diagnostics.Process]::GetProcessById(0)
+
+#$parameters = @{
+#Path = "c:\temp"
+#Recurse = $true
+#}
+
+#$scriptBlock = { "Hello World" }
+
+$xml='<AddressBook>
+<Person contactType="Personal">
+<Name>Lee</Name>
+<Phone type="home">555-1212</Phone>
+<Phone type="work">555-1213</Phone>
+</Person>
+<Person contactType="Business">
+<Name>Ariel</Name>
+<Phone>555-1234</Phone>
+</Person>
+</AddressBook>'
+
+#$xml | Select-Xml "/" | Select -Expand Node
+
+#$xml | Select-Xml "/AddressBook" |Select -Expand Node
+
+#$xml | Select-Xml "//Phone" | Select -Expand Node
+
+#$xml | Select-Xml "//Phone/.." | Select -Expand Node
+
+$xml | Select-Xml "//Phone/@type" |Select -Expand Node
+
+
---
+++ PowerShellTest/PowerSchellInPractice/Start.ps1
@@ -0,0 +1,89 @@
+#
+# Start.ps1
+#
+$proc=Get-Process
+#$proc| Where-Object {$proc.Handles -gt 500}
+#$proc|Where-Object {$_.CPU -gt 100}
+
+#$proc|Sort-Object -Property WS -Descending | Select-Object -First 5
+
+##Listing 1.3 Comparing files
+#Compare -ReferenceObject chap01v1.txt -DifferenceObject chap01v2.txt
+
+#Listing 1.8 Using a calculated property in Select-Object
+#$now=Get-Date
+#Get-Process|Where-Object {$_.StartTime} |Select Name, @{Name="Run Time";Expression={[int]($now - $_.StartTime).TotalMinutes}} | Sort "Run Time" -Descending | Format-Table –AutoSize
+
+#	Get-ChildItem "C:\Windows" | Where{!$_.PsIsContainer} |
+#		ForEach-Object {
+#		if ($_.Length -gt 1MB) {
+#		Remove-Item $_.Fullname -WhatIf
+#		}
+#		elseif ($_.Length -gt 0.5MB){
+#		Write-Host $_.Name, $_.Length, `
+#		" will be removed if more space required" `
+#		-ForegroundColor Yellow
+#		}
+#		else{
+#		if ($_.Length -gt 0.25MB){
+#		Write-Host $_.Name, $_.Length, `
+#		" will be removed in third wave" `
+#		-ForegroundColor Blue
+#		}
+#	}
+#}
+
+#2.7.
+#Language features 51
+#$Out1 = @"
+#$count files are smaller than 1MB and occupy $total_size bytes
+#"@
+#$Out2 = @"
+#$count_big files are bigger than 1MB and occupy $total_size_big bytes
+#"@
+#Get-ChildItem "c:\Windows" |
+# ForEach-Object `
+#-Begin {
+#$count = 0
+#$total_size = 0
+#$count_big = 0
+#$total_size_big = 0
+#} `
+#-Process {
+#if ($_.Length -gt 1MB) {
+#$total_size_big += $_.Length
+#$count_big ++
+#}
+#else {
+#$total_size += $_.Length
+#$count ++
+#}
+#} `
+#-End {
+#Write-Host $out1
+#Write-Host $out2
+#}
+
+#Listing 2.8 Using a foreach loop
+#$date = (Get-Date).Adddays(-10)
+#$files = Get-ChildItem "c:\Windows"
+#	foreach($file in $files){
+#	if ($file.LastAccessTime -lt $date){
+#		Write-Host $file
+#	#Remove-Item -Path $file.FullName -WhatIf
+#	}
+#}
+
+#Listing 2.9 Creating test folders and files with loops
+
+$data = 1..57
+$j = 1
+while ($j -le 10){
+$foldername = "Testfolder_$j"
+New-Item -ItemType directory -Name $foldername
+$j++
+for ($i=0; $i -le 10; $i++){
+$filename = "file_$i.txt"
+	Set-Content -Path "$foldername\$filename" -Value $data
+}
+}
